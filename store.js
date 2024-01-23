4e46b6f03f0f946417f640bb16ca6174381694f2
siddhiBhosale18 <siddhibhosale1830@gmail.com>
2024-01-24 00:48:07 +0530

    till render

---
+++ store.js
@@ -0,0 +1,21 @@
+const store = {
+    todos: [{
+            id: "1",
+            title: "Complete Task A",
+            completed: false,
+        },
+        {
+            id: "3",
+            title: "Read Book",
+            completed: true,
+        },
+        {
+            id: "1",
+            title: "Write Code",
+            completed: true,
+        },
+    ],
+};
+
+
+export default store;
