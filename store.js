e57b99ecb6346ad8ebcda0a33c7e8a55d2b2ca79
siddhiBhosale18 <siddhibhosale1830@gmail.com>
2024-01-29 16:25:54 +0530

    Completed

--- store.js
+++ store.js
@@ -5,17 +5,47 @@ const store = {
             completed: false,
         },
         {
-            id: "3",
+            id: "2",
             title: "Read Book",
-            completed: true,
-        },
-        {
-            id: "1",
-            title: "Write Code",
             completed: true,
         },
     ],
 };
+const storeHandler = {
+    get(target, property) {
+        return target[property];
+    },
+    set(target, property, value) {
+        target[property] = value;
+        if (property == "todos") {
+            window.dispatchEvent(new Event("todoschange"));
+        }
+        localStorage.setItem("store", JSON.stringify(store));
+        return true;
+    },
+};
 
+// traps
 
-export default store;
+const storeProxy = new Proxy(store, storeHandler);
+
+function addTodo(newTodo) {
+    storeProxy.todos = [...storeProxy.todos, newTodo];
+}
+
+function deleteTodo(id) {
+    storeProxy.todos = storeProxy.todos.filter((todo) => todo.id !== id);
+}
+
+function toggleCompleted(id, completed) {
+    storeProxy.todos = storeProxy.todos.map((todo) => {
+        if (todo.id === id) {
+            return {...todo, completed: completed };
+        } else {
+            return todo;
+        }
+    });
+}
+
+export { addTodo, deleteTodo, toggleCompleted };
+export default storeProxy;
